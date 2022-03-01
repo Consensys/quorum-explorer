d3dcd4ec14f8b590f2f6b4ef6e0b18c65d4d8884
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-03-01 16:34:19 +1000

    clean just html

--- src/components/Contracts.js
+++ src/components/Contracts.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
-import Container from '@mui/material/Container';
 
 function Contracts(){
 
   return (
-    <Container maxWidth="xl">
-    
-    <Typography
-      component="h1"
-      variant="h2"
-      align="center"
-      color="text.primary"
-      gutterBottom
-    >
-      Contracts
-    </Typography>
-    
-  </Container>
-
+    <h2> Contracts</h2>
   );
+  
 }
-
 export default Contracts;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,40 +1,11 @@
 import React, { Component } from 'react';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
-import Container from '@mui/material/Container';
-import { Stack } from '@mui/material';
 
 function Dashboard(){
 
   return (
-    <Container maxWidth="xl">
-    
-    <Typography
-      component="h1"
-      variant="h2"
-      align="center"
-      color="text.primary"
-      gutterBottom
-    >
-      Dashboard
-    </Typography>
-    <Typography variant="h5" align="center" color="text.secondary" paragraph>
-      Something short and leading about the collection below—its contents,
-      the creator, etc. Make it short and sweet, but not too short so folks
-      don&apos;t simply skip over it entirely.
-    </Typography>
-    <Stack
-      sx={{ pt: 4 }}
-      direction="row"
-      spacing={2}
-      justifyContent="center"
-    >
-      <Button variant="contained">Main call to action</Button>
-      <Button variant="outlined">Secondary action</Button>
-    </Stack>
-  </Container>
-
+    <h2> Dashboard</h2>
   );
+  
 }
 
 export default Dashboard;
