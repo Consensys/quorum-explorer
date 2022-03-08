f3484c690029149616dc3985e1845968393b07ca
Eric Lin <eric.lin@consensys.net>
2022-03-08 17:05:52 +1000

    more animations
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- src/components/Header/PageHeader.jsx
+++ src/components/Header/PageHeader.jsx
@@ -5,7 +5,11 @@ import { motion } from "framer-motion";
 export default function PageHeader({ HeadingName }) {
   return (
     <>
-      <motion.center initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <motion.center
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}
+      >
         <Heading as="h1" size="xl" m={3}>
           {HeadingName}
         </Heading>
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,26 +5,33 @@ import React from "react";
 import { data } from "./header";
 import MobileNav from "./MobileNav";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { motion } from "framer-motion";
 
 export default function NavBar() {
   return (
-    <chakra.header id="header">
-      <Flex w="100%" px="6" py="5" align="center" justify="space-between">
-        <QuorumIcon />
-        <HStack spacing="5" display={{ base: "none", md: "flex" }}>
-          {data.map((item, i) => (
-            <Link key={i} to={item["label"].toLowerCase()}>
-              <Button leftIcon={item["icon"]} variant="ghost">
-                {item.label}
-              </Button>
-            </Link>
-          ))}
-        </HStack>
-        <HStack>
-          <MobileNav />
-          <ColorModeSwitcher justifySelf="flex-end" />
-        </HStack>
-      </Flex>
-    </chakra.header>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <chakra.header id="header">
+        <Flex w="100%" px="6" py="5" align="center" justify="space-between">
+          <QuorumIcon />
+          <HStack spacing="5" display={{ base: "none", md: "flex" }}>
+            {data.map((item, i) => (
+              <Link key={i} to={item["label"].toLowerCase()}>
+                <Button leftIcon={item["icon"]} variant="ghost">
+                  {item.label}
+                </Button>
+              </Link>
+            ))}
+          </HStack>
+          <HStack>
+            <MobileNav />
+            <ColorModeSwitcher justifySelf="flex-end" />
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </motion.div>
   );
 }
