c031aa5a5aafb08e46dac560d27b2bca34ad0a2c
Eric Lin <eric.lin@consensys.net>
2022-03-08 16:51:45 +1000

    add framer motion to header test
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- src/components/Header/PageHeader.jsx
+++ src/components/Header/PageHeader.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Heading, Divider } from '@chakra-ui/react';
+import React from "react";
+import { Heading, Divider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 export default function PageHeader({ HeadingName }) {
   return (
     <>
-      <center>
+      <motion.center initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
         <Heading as="h1" size="xl" m={3}>
           {HeadingName}
         </Heading>
         <Divider maxW="40vw" />
-      </center>
+      </motion.center>
     </>
   );
 }
