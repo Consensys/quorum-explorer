e359251ce20c4f39b072a35eb53349e5c63864f3
Eric Lin <eric.lin@consensys.net>
2022-05-16 14:26:09 +1000

    working auth
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- src/common/components/Misc/AccessDenied.tsx
+++ src/common/components/Misc/AccessDenied.tsx
@@ -1,20 +1,46 @@
 import { signIn } from "next-auth/react";
+import { Container, Heading, Center, VStack, Button } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 export default function AccessDenied() {
   return (
     <>
-      <h1>Access Denied</h1>
-      <p>
-        <a
-          href="/api/auth/signin"
-          onClick={(e) => {
-            e.preventDefault();
-            signIn();
-          }}
-        >
-          You must be signed in to view this page
-        </a>
-      </p>
+      <motion.section
+        initial={{ opacity: 0 }}
+        whileInView="visible"
+        viewport={{ once: true }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Container>
+          <Center>
+            <VStack>
+              <Heading as="h1" size="4xl" noOfLines={1}>
+                Access Denied
+              </Heading>
+              <a
+                href="/api/auth/signin"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                You must be signed in to view this page
+              </a>
+              <Button
+                colorScheme="blue"
+                // href="/api/auth/signin"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                Sign in
+              </Button>
+            </VStack>
+          </Center>
+        </Container>
+      </motion.section>
     </>
   );
 }
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,27 +9,27 @@ import Auth0Provider from "next-auth/providers/auth0";
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+    // TwitterProvider({
+    //   clientId: process.env.TWITTER_ID,
+    //   clientSecret: process.env.TWITTER_SECRET,
+    // }),
+    // Auth0Provider({
+    //   clientId: process.env.AUTH0_ID,
+    //   clientSecret: process.env.AUTH0_SECRET,
+    //   issuer: process.env.AUTH0_ISSUER,
+    // }),
   ],
   theme: {
     colorScheme: "light",
